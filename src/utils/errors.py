e609e027aac624f5d6e59744d3c269838cdeb40b
Abdulmunim <66489217+abdulmunimjemal@users.noreply.github.com>
2025-05-02 15:59:30 +0300

    [ENH] Add Errors

---
+++ src/utils/errors.py
@@ -0,0 +1,5 @@
+class LatexCompilationError(Exception):
+    def __init__(self, message, stdout=None, stderr=None):
+        super().__init__(message)
+        self.stdout = stdout
+        self.stderr = stderr
